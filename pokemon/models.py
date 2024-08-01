bc58210d9e111b73d2a22ea4f34956abb945568d
halex22 <73904950+halex22@users.noreply.github.com>
2024-08-01 22:52:41 +0200

    minor models fixes

--- pokemon/models.py
+++ pokemon/models.py
@@ -13,11 +13,11 @@ class Ability(models.Model):
 
 class Pokemon(models.Model):
     name = models.CharField(unique=True, max_length=100, null=False, blank=False)
-    heigth = models.DecimalField(decimal_places=3, max_digits=2, null=False, blank=False)
-    weigth = models.DecimalField(decimal_places=3, max_digits=2, null=False, blank=False)
+    heigth = models.DecimalField(decimal_places=2, max_digits=4, null=False, blank=False)
+    weigth = models.DecimalField(decimal_places=2, max_digits=4, null=False, blank=False)
     types = models.ManyToManyField(PokemonType, related_name='types')
     abilities = models.ManyToManyField(Ability, related_name='abilities')
-    hidden_ability = models.ForeignKey(Ability, on_delete=models.SET_NULL, related_name='hidden_ability')
+    hidden_ability = models.ForeignKey(Ability, on_delete=models.SET_NULL, related_name='hidden_ability', null=True)
 
 
 class BaseStats(models.Model):
@@ -36,6 +36,7 @@ class SmallNameModel(models.Model):
     class Meta:
         abstract = True
 
+
 class FriendShipCategory(SmallNameModel):
     ...
 
@@ -46,10 +47,10 @@ class GrowthRateCategory(SmallNameModel):
 class Training(models.Model):
     pokemon = models.ForeignKey(Pokemon, on_delete=models.CASCADE, related_name='traning')
     base_exp = models.PositiveIntegerField(null= False, blank=False)
-    catch_rate = models.DecimalField(decimal_places=3, max_digits=2, null=False, blank=False)
-    friendship_category = models.ForeignKey(FriendShipCategory, on_delete=models.SET_NULL, related_name='friendship_category')
+    catch_rate = models.DecimalField(decimal_places=2, max_digits=3, null=False, blank=False)
+    friendship_category = models.ForeignKey(FriendShipCategory, on_delete=models.SET_NULL, related_name='friendship_category', null=True)
     base_friendship = models.PositiveIntegerField()
-    growth_rate = models.ForeignKey(GrowthRateCategory, on_delete=models.SET_NULL, related_name='growth_rate')
+    growth_rate = models.ForeignKey(GrowthRateCategory, on_delete=models.SET_NULL, related_name='growth_rate', null=True)
 
 
 class EggGroup(SmallNameModel):
@@ -66,7 +67,7 @@ class Breeding(models.Model):
     male = models.DecimalField(max_digits=3, decimal_places=2, blank=True, null=True)
     female = models.DecimalField(max_digits=3, decimal_places=2, blank=True, null=True)
     egg_groups = models.ManyToManyField(EggGroup, related_name='egg_groups')
-    egg_cycles_category = models.ForeignKey(EggCyclesCategory, on_delete=models.SET_NULL, related_name='egg_cycles_category')
+    egg_cycles_category = models.ForeignKey(EggCyclesCategory, on_delete=models.SET_NULL, related_name='egg_cycles_category', null=True)
     egg_cycles_value = models.PositiveIntegerField()
 
 
